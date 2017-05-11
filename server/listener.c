c0316c0c1148e29c1ba4a3bd3aa828188b551f50
Jerzy Jaśkiewicz <jurek.jaskiewicz@gmail.com>
2017-05-11 21:17:00 +0200

    Commit testowy

--- server/listener.c
+++ server/listener.c
@@ -1,6 +1,6 @@
 #pragma once
-#include <unistd.h> 
-#include <stdio.h>  
+#include <unistd.h>
+#include <stdio.h>
 #include <string.h>
 #include <stdlib.h>
 #include <fcntl.h>
@@ -27,13 +27,13 @@ int create_listener(int port, int max_clients)
 	server_addr.sin_addr.s_addr = INADDR_ANY; 
 	if (setsockopt(server_fd, SOL_SOCKET,  SO_REUSEADDR, &(int){1}, sizeof(int)) < 0 )
 	{
-		perror("SetSockOpt  error");
+		perror("setsockopt  error");
 		return EXIT_CANNOT_CONFIGURE;
 	}
 	// if (ioctl(server_fd, FIONBIO, &(int){1}) , 0)
 	//  	return EXIT_CANNOT_CONFIGURE;
 	if (bind(server_fd, (struct sockaddr*)&server_addr, sockaddr_in_size)<0){
-		perror("Bind error");
+		perror("bind error");
 		return EXIT_CANNOT_BIND;
 	}
 	if (listen(server_fd, max_clients) < 0)
@@ -45,8 +45,8 @@ int create_listener(int port, int max_clients)
 		client_addr.sin_family = AF_INET;
 		int client_addr_len = sizeof(client_addr);
 		int client_fd = accept(server_fd, (struct sockaddr*)&client_addr, &client_addr_len);
-		printf("Client accepted with fd = %d", client_fd);			
-		if (client_fd > 0) 
+		printf("Client accepted with fd = %d", client_fd);
+		if (client_fd > 0)
 		{
 			service_client(client_fd);
 		}
